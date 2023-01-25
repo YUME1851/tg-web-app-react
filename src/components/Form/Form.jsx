0d225a52d31cf269dbcf5cc9934c2fa35a380d94
Ekaterina S <freakkatekim@gmail.com>
2023-01-25 17:57:42 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Form.css';
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
     const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
