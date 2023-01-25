0308a36187efad9e309dd5f9357efb6f53f64187
Ekaterina S <freakkatekim@gmail.com>
2023-01-25 13:50:57 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
+
 function App() {
 
     useEffect(() => {
         tg.ready(); //Сообщает о том, что приложение полностью проинициализировалось и его можно отрисовывать
     }, [])
 
-    const onClose = () => {
-        tg.close()
-    }
+
 
   return (
     <div className="App">
-      oh fuck
 
-        <button onClick={onClose}>Мяу!</button>
+
     </div>
   );
 }
