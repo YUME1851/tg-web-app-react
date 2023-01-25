f3343975fe0ec5910e2a95a22797013576549576
Ekaterina S <freakkatekim@gmail.com>
2023-01-25 14:58:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
+
 
     useEffect(() => {
         tg.ready(); //Сообщает о том, что приложение полностью проинициализировалось и его можно отрисовывать
     }, [])
 
-
-
   return (
     <div className="App">
-
-
+        <button onClick={onToggleButton}>Жмяк-с!</button>
     </div>
   );
 }
